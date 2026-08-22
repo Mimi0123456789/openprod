95a04a269a2f1e312a847b17ea607ce52481c94c
Mimi0123456789 <adressepoubelle868@gmail.com>
2026-08-22 18:32:55 +0200

    Corrections 2

--- dao/database.php
+++ dao/database.php
@@ -31,18 +31,16 @@ class database
 
             $dsn = "mysql:host={$host};port={$port};dbname={$db_name};charset={$charset}";
 
-            $this->bdd = new PDO(
-                $dsn,
-                $user,
-                $password,
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_PERSISTENT => false,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::MYSQL_ATTR_INIT_COMMAND =>
-                        "SET NAMES {$charset} COLLATE {$collate}"
-                ]
-            );
+			$this->bdd = new PDO(
+				$dsn,
+				$user,
+				$password,
+				[
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+					PDO::ATTR_PERSISTENT => false,
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+				]
+			);
 
         } catch (PDOException $e) {
             error_log("Erreur connexion BDD : " . $e->getMessage());
